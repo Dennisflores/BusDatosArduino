a3562d077908c6b5f7ab8576dfbc640d2df3a5b5
henryherrera15 <henryconcoha15@gmail.com>
2018-03-10 10:10:19 -0600

    encendiendo luz de arduino

--- busdatos_Arduino/busdatos_Arduino.ino
+++ busdatos_Arduino/busdatos_Arduino.ino
@@ -1,9 +1,19 @@
 void setup() {
 Serial.begin(9600); 
 Serial.println("hola soy un arduino");
+pinMode(13,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ if(Serial.available()){
+char Letra=Serial.read();
+Serial.print(Letra);
+if(Letra == 'H') {
+  digitalWrite(13,1);
+}
 
+else if (Letra == 'L'){
+  digitalWrite(13,0);
+}
+}
 }
